c7d2508a1442e64a3ca8b68432c1722406d4f831
Megan Anthony <megananthony@Megans-MacBook-Air.local>
2018-08-06 12:00:45 -0400

    server file started

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var userArray = {
+    name: "name",
+    photo: "pic",
+    scores: [
+        Q1,
+        Q2,
+        Q3,
+        Q4,
+        Q5,
+        Q6,
+        Q7,
+        Q8,
+        Q9,
+        Q10
+    ]
+}
+
+modules.export = userArray;
---
+++ server.js
@@ -0,0 +1,28 @@
+var path = require('path')
+var express = require('express')
+var bodyParser = require('body-parser')
+ 
+var app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
+
+app.listen(3000)
+console.log("listening to 3000");
