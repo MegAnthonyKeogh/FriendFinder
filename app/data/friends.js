4f7153719d6be921c3b2bc64e4ea7c64d97f1c03
Megan Anthony <megananthony@Megans-MacBook-Air.local>
2018-08-09 23:54:51 -0400

    coming together

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,17 +2,17 @@ var users = [{
     name: "Megan",
     photo: "pic",
     scores: [
-        Q1 = 0,
-        Q2 = 0,
-        Q3 = 0,
-        Q4 = 0,
-        Q5 = 0,
-        Q6 = 0,
-        Q7 = 0,
-        Q8 = 0,
-        Q9 = 0,
-        Q10 = 0
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
     ]
-  }];
+}];
 
-module.export = users;
+module.exports = users;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,62 +3,61 @@ var newUser = null;
 var result = null;
 var winner = null;
 
-console.log(users);
 
-// function theBigCompare(newUser) {
-//     for (i = 0; i < users.length; i++) {
-//         var newarray = Number(newUser.scores).map(function (item, index) {
+function theBigCompare(newUser) {
+    for (i = 0; i < users.length; i++) {
+        var newarray = newUser.scores.map(function (item, index) {
 
-//             if (item > users[i].scores[index]) {
+            if (item > users[i].scores[index]) {
 
-//                 return item - users[i].scores[index];
+                return item - users[i].scores[index];
 
-//             } else {
-//                 return users[i].scores[index] - item;
-//             }
-//         })
-//         console.log(newarray);
+            } else {
+                return users[i].scores[index] - item;
+            }
+        })
+        console.log(newarray);
 
-//         var sum = null;
+        var sum = null;
 
-//         const reducer = (accumulator, currentValue) => accumulator + currentValue;
-//         sum = newarray.reduce(reducer);
-//         console.log("sum = " + sum);
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        sum = newarray.reduce(reducer);
+        console.log("sum = " + sum);
 
-//         if (result === null) {
-//             result = sum;
-//             console.log("first match is " + users[i].name + " with a sum of " + sum);
+        if (result === null) {
+            result = sum;
+            console.log("first match is " + users[i].name + " with a sum of " + sum);
 
-//         } else if (result > sum) {
-//             result = sum;
-//             winner = users[i];
-//             console.log("new match" + users[i].name + "with a sum of " + sum);
+        } else if (result > sum) {
+            result = sum;
+            winner = users[i];
+            console.log("new match" + users[i].name + "with a sum of " + sum);
 
-//         } else if (result < sum) {
+        } else if (result < sum) {
 
-//             console.log("do nothing");
+            console.log("do nothing");
 
-//         } else {
-//             console.log("not a better match, " + users[i].name + "is still the better match " + result);
-//         }
-//     }
-// }
+        } else {
+            console.log("not a better match, " + users[i].name + "is still the better match " + result);
+        }
+    }
+    console.log(result);
+    console.log(winner);
 
-// console.log(result);
-// console.log(winner);
+}
+
 
 module.exports = function (app) {
 
     app.get("/api/friends", function (req, res) {
         return res.json(users);
-        
 
     });
 
     app.post("/api/friends", function (req, res) {
         newUser = req.body;
-        theBigCompare();
-        //users.push(newUser);
+        theBigCompare(newUser);
+        users.push(newUser);
         return res.json(newUser);
 
     });
